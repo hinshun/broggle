6b035bfff32a3e281e8e4b139346154a9582de95
Edgar <edgarhinshunlee@gmail.com>
2014-07-19 10:47:15 -0400

    Clean up spec and rails helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,14 +4,8 @@ require File.expand_path("../dummy-app/config/environment.rb",  __FILE__)
 require 'rspec/rails'
 require 'factory_girl_rails'
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+Dir[Broggle::Engine.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 Rails.backtrace_cleaner.remove_silencers!
 
@@ -19,6 +13,5 @@ RSpec.configure do |config|
   config.mock_with :rspec
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
-  config.order = "random"
   config.include FactoryGirl::Syntax::Methods
 end
