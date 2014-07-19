4ac820219a126979537dfd44f00cf981dcbbcc34
Edgar <edgarhinshunlee@gmail.com>
2014-07-19 10:47:58 -0400

    Wrote specs for broggle model

--- spec/models/broggle/broggle_spec.rb
+++ spec/models/broggle/broggle_spec.rb
@@ -1,7 +1,139 @@
-describe Broggle do
-  let(:broggle) { build(:broggle) }
+describe Broggle::Broggle do
+  let(:broggle) { build :broggle }
+  before(:example) { RuggedSpecHelper.prepare_repo }
 
-  it 'has 5 branches' do
-    expect(broggle.repo.branches.count).to eq(5)
+  describe '#repo' do
+    subject { broggle.repo }
+    it { is_expected.to be_a Rugged::Repository }
+  end
+
+  describe '#branches' do
+    subject { broggle.branches }
+
+    context ':name' do
+      subject { super().map(&:name) }
+
+      context 'when initialized' do
+        it { is_expected.to include 'master' }
+      end
+
+      context 'when creating a new branch' do
+        before { broggle.repo.create_branch('branch') }
+        it { is_expected.to include 'branch' }
+      end
+
+      context 'when creating a new broggle' do
+        before { broggle.create_broggle('master') }
+        it 'should not include any that starts with "broggle_"' do
+          is_expected.to_not include start_with 'broggle_'
+        end
+      end
+    end
+  end
+
+  describe '#current_branch' do
+    subject { broggle.current_branch }
+
+    context 'when on `master`' do
+      before { broggle.repo.checkout('master') }
+      it { is_expected.to eq 'master' }
+    end
+
+    context 'when repository is headless' do
+      before do
+        head_oid = broggle.repo.head.target.oid
+        broggle.repo.checkout(head_oid)
+      end
+      it { is_expected.to eq 'HEAD' }
+    end
+  end
+
+  describe '#deploy' do
+    subject { broggle.deploy(branches) }
+    let(:branches) { {base: 'master', merges: []} }
+
+    it do
+      expect {
+        subject
+      }.to change(broggle, :current_branch).to start_with 'broggle_'
+    end
+  end
+
+  describe '#create_broggle' do
+    subject { broggle.create_broggle('master') }
+
+    it 'creates a branch' do
+      expect { subject }.to change(broggle.repo.branches, :count).by 1
+    end
+
+    context 'broggle branch name' do
+      subject { super().name }
+      it { is_expected.to start_with 'broggle_' }
+    end
+  end
+
+  describe '#merge' do
+    subject { broggle.merge(branch) }
+    let(:branch) { RuggedSpecHelper.current_branch }
+    before do
+      RuggedSpecHelper.create_scenario(scenario)
+      broggle_branch = broggle.create_broggle('master')
+      broggle.repo.checkout(broggle_branch)
+    end
+
+    context 'when there are no conflicts' do
+      let(:scenario) { :no_conflict }
+      it { is_expected.to be :merge_success }
+    end
+
+    context 'when there are conflicts' do
+      let(:scenario) { :conflict }
+      it { is_expected.to be :merge_conflict }
+    end
+  end
+
+  describe '#branches_substring_search' do
+    subject { broggle.branches_substring_search(query) }
+
+    context 'when searching for `ma`' do
+      let(:query) { 'ma' }
+      it { is_expected.to include 'master' }
+    end
+
+    context 'when searching for `mastre`' do
+      let(:query) { 'mastre' }
+      it { is_expected.to be_empty }
+    end
+
+    context 'when searching for `maer`' do
+      let(:query) { 'maer' }
+      it { is_expected.to be_empty }
+    end
+  end
+
+  describe '#branches_flex_score' do
+    subject { broggle.branches_flex_score(query) }
+
+    let(:query) { '' }
+    it { is_expected.to all include :score }
+
+    context 'when scoring for `ma`' do
+      let(:query) { 'ma' }
+      it 'should include {name: "master"}' do
+        is_expected.to include(include name: 'master')
+      end
+    end
+
+    context 'when scoring for `mastre`' do
+      let(:query) { 'mastre' }
+      it { is_expected.to be_empty }
+    end
+
+    context 'when scoring for `maer`' do
+      let(:query) { 'maer' }
+      it 'should include {name: "master"}' do
+        is_expected.to include(include name: 'master')
+      end
+    end
   end
 end
---
+++ spec/support/helpers/rugged_spec_helper.rb
@@ -0,0 +1,100 @@
+module RuggedSpecHelper
+  module_function
+
+  require 'rspec/mocks/standalone'
+
+  WORK_DIR = Broggle::Engine.root.join("spec/dummy-git/")
+  BROGGLER = { name: "Broggle", email: "" }
+  BASE_COMMIT_OPTIONS = {
+    author: BROGGLER,
+    commiter: BROGGLER,
+    update_ref: 'HEAD'
+  }
+  GIT_FILEMODE_BLOB = 0100644
+
+  def repo
+    @repo ||= Rugged::Repository.new(WORK_DIR.to_s)
+  end
+
+  def current_branch
+    @current_branch
+  end
+
+  def current_branch=(name)
+    @current_branch = name
+  end
+
+  def prepare_repo
+    stub_const "Broggle::Broggle::GIT_PATH", WORK_DIR.to_s
+
+    FileUtils.rm_r Dir.glob(WORK_DIR.join('*'))
+
+    git_dir = WORK_DIR.join '.git'
+    FileUtils.rm_rf git_dir if Dir.exists? git_dir
+
+    new_repo = Rugged::Repository.init_at(WORK_DIR.to_s)
+
+    index = new_repo.index
+    commit(new_repo, index.write_tree(new_repo), 'Initial commit')
+  end
+
+  def create_scenario(scenario)
+    case scenario
+    when :no_conflict
+      create_no_conflict_scenario
+    when :conflict
+      create_conflict_scenario
+    end
+  end
+
+  def create_no_conflict_scenario
+    repo.checkout('master')
+    write_file_to_repo(repo, 'filename',  'content')
+
+    self.current_branch = 'no_conflict_branch'
+    repo.create_branch(current_branch)
+    repo.checkout(current_branch)
+    write_file_to_repo(repo, 'filename', 'new content')
+
+    repo.checkout('master', strategy: :force)
+  end
+
+  def create_conflict_scenario
+    repo.checkout('master')
+    write_file_to_repo(repo, 'filename', 'content')
+
+    self.current_branch = 'conflict_branch'
+    repo.create_branch(current_branch)
+    repo.checkout(current_branch)
+    write_file_to_repo(repo, 'filename', 'new content')
+
+    repo.checkout('master', strategy: :force)
+    write_file_to_repo(repo, 'filename', 'conflict')
+  end
+
+  def write_file_to_repo(repo, filename, content)
+    builder = Rugged::Tree::Builder.new
+    builder << {
+      type: :blob,
+      name: filename,
+      oid: repo.write(content, :blob),
+      filemode: GIT_FILEMODE_BLOB
+    }
+
+    commit(repo, builder.write(repo), "Wrote #{content} to #{filename}")
+  end
+
+  def commit(repo, tree, message)
+    commit_options = BASE_COMMIT_OPTIONS.merge(
+      parents: repo.empty? ? [] : [repo.head.target].compact,
+      tree: tree,
+      message: message
+    )
+
+    Rugged::Commit.create(repo, commit_options)
+  end
+
+  def broggle_branch
+    repo.branches.select { |b| b.name.start_with?('broggle_') }.first.name
+  end
+end
